79628ba53537d83e88b1c61c20dacacd9aa1db49
seggy14 <skamat36@gatech.edu>
2021-10-22 23:03:37 -0400

    w

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import flask
+from flask import Flask, send_from_directory
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return send_from_directory("front_end", "homepage.html")
+    # return "Hello world"
